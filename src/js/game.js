c8e52279e5f45d720e38c4a10a59a4f293a49591
jeff9912 <1095143@hr.nl>
2025-05-06 16:11:03 +0200

    commit

--- src/js/game.js
+++ src/js/game.js
@@ -5,28 +5,106 @@ import { Resources, ResourceLoader } from './resources.js'
 export class Game extends Engine {
 
     constructor() {
-        super({ 
+        super({
             width: 1280,
             height: 720,
             maxFps: 60,
             displayMode: DisplayMode.FitScreen
-         })
+        })
         this.start(ResourceLoader).then(() => this.startGame())
     }
 
     startGame() {
         console.log("start de game!")
-        const fish = new Actor()
-        fish.graphics.use(Resources.Fish.toSprite())
-        fish.pos = new Vector(500, 300)
-        fish.vel = new Vector(-10,0)
-        fish.events.on("exitviewport", (e) => this.fishLeft(e))
-        this.add(fish)
+
+        // for (let i = 0; i < 1000; i++) {
+        //     const fish = new Actor()
+        //     fish.graphics.use(Resources.Fish.toSprite())
+        //     fish.pos = new Vector(Math.random() * 1280, Math.random() * 720)
+        //     fish.vel = new Vector(Math.random() * 200 - 100 , Math.random() * 200 - 100)
+        // fish.events.on("exitviewport", (e) => this.fishLeft(e))
+        //     this.add(fish)
+        // }
+
+
+
+
+        //bg 
+        const bg = new Actor()
+        const sprite = Resources.bliss.toSprite()
+        bg.graphics.use(sprite)
+        bg.pos = new Vector(0, 0)
+        bg.scale = new Vector(10, 6)
+        this.add(bg)
+
+
+        for (let i = 0; i < 3; i++) {
+            const fish = new Actor()
+            fish.graphics.use(Resources.Jonesy.toSprite())
+            fish.pos = new Vector(Math.random() * 1280, Math.random() * 720)
+            fish.vel = new Vector(Math.random() * 200 - 100, Math.random() * 200 - 100)
+            fish.scale = new Vector(0.2, 0.2)
+            this.add(fish)
+            fish.events.on("exitviewport", (e) => this.fishLeft(e))
+        }
+
+        for (let i = 0; i < 3; i++) {
+            const fish = new Actor()
+            fish.graphics.use(Resources.sans.toSprite())
+            fish.pos = new Vector(Math.random() * 1280, Math.random() * 720)
+            fish.vel = new Vector(Math.random() * 200 - 100, Math.random() * 200 - 100)
+            fish.scale = new Vector(0.2, 0.2)
+            this.add(fish)
+            fish.events.on("exitviewport", (e) => this.fishLeft(e))
+        }
+
+        for (let i = 0; i < 5; i++) {
+            const fish = new Actor()
+            fish.graphics.use(Resources.shark.toSprite())
+            fish.pos = new Vector(Math.random() * 1280, Math.random() * 720)
+            fish.vel = new Vector(Math.random() * 200 - 100, Math.random() * 200 - 100)
+            fish.scale = new Vector(0.2, 0.2)
+            this.add(fish)
+            fish.events.on("exitviewport", (e) => this.fishLeft(e))
+        }
+
+        for (let i = 0; i < 10; i++) {
+            const fish = new Actor()
+            fish.graphics.use(Resources.spongebob.toSprite())
+            fish.pos = new Vector(Math.random() * 1280, Math.random() * 720)
+            fish.vel = new Vector(Math.random() * 200 - 100, Math.random() * 200 - 100)
+            fish.scale = new Vector(0.4, 0.4)
+            this.add(fish)
+            fish.events.on("exitviewport", (e) => this.fishLeft(e))
+        }
+
+        for (let i = 0; i < 10; i++) {
+            const fish = new Actor()
+            fish.graphics.use(Resources.patrick.toSprite())
+            fish.pos = new Vector(Math.random() * 1280, Math.random() * 720)
+            fish.vel = new Vector(Math.random() * 200 - 100, Math.random() * 200 - 100)
+            fish.scale = new Vector(0.3, 0.3)
+            this.add(fish)
+            fish.events.on("exitviewport", (e) => this.fishLeft(e))
+        }
+
+        for (let i = 0; i < 10; i++) {
+            const fish = new Actor()
+            fish.graphics.use(Resources.squidward.toSprite())
+            fish.pos = new Vector(Math.random() * 1280, Math.random() * 720)
+            fish.vel = new Vector(Math.random() * 200 - 100, Math.random() * 200 - 100)
+            fish.scale = new Vector(0.08, 0.08)
+            this.add(fish)
+            fish.events.on("exitviewport", (e) => this.fishLeft(e))
+        }
+
     }
 
     fishLeft(e) {
-        e.target.pos = new Vector(1350, 300)
+        e.target.pos = new Vector(Math.random() * 1280, Math.random() * 720)
     }
+
+
 }
 
 new Game()
--- src/js/resources.js
+++ src/js/resources.js
@@ -2,7 +2,14 @@ import { ImageSource, Sound, Resource, Loader } from 'excalibur'
 
 // voeg hier jouw eigen resources toe
 const Resources = {
-    Fish: new ImageSource('images/fish.png')
+    Fish: new ImageSource('images/fish.png'),
+    Jonesy: new ImageSource('images/Survivalist_Jonesy_Epic.png'),
+    bliss: new ImageSource('images/bliss.png'),
+    sans: new ImageSource('images/sans.png'),
+    shark: new ImageSource('images/shark.png'),
+    squidward: new ImageSource('images/squidward.png'),
+    spongebob: new ImageSource('images/spongebob.png'),
+    patrick: new ImageSource('images/patrick.png'),
 }
 
 
